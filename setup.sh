f5ed9c888c2b907dd25c0870c9c6815cd8d8432d
Romakumbhalwar <romalk2001@gmail.com>
2025-05-27 14:46:31 +0530

    Fix: added Streamlit run command to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Create the .streamlit directory and config file
 mkdir -p ~/.streamlit/
 
 echo "\
@@ -6,3 +9,6 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 " > ~/.streamlit/config.toml
+
+# Run the Streamlit app
+streamlit run app.py
